de15cdf0fdc4efcb4c9ff9eb5f3ffc513bc23dba
vladmatlakh <vlad.matlakh1@gmail.com>
2026-03-25 13:40:56 +0200

    third commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    describe = models.TextField(max_length=300, blank=True, null=True)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    item_number = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
